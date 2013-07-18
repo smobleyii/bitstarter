5c608c25dda3614d3f6ce1d0b1ba9fc28a5c7544
Sylvester Mobley <smobleyii@gmail.com>
2013-07-18 21:48:27 +0000

    Updated index.html and web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/index.html', function(request, response) {
+  var content = fs.readFileSync('index.html','utf8');
+    response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
